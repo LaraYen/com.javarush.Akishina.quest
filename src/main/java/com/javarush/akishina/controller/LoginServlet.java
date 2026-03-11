0976011656b62dea548142af823d008f68d862b2
Alina Leontyeva Default GITLAB Profile <Alinokleo@gmail.com>
2026-03-12 01:02:25 +0300

    Add util class QuestSession, change the way to navigate to a new quest scene, add logging, add win/loss tracking

--- src/main/java/com/javarush/akishina/controller/LoginServlet.java
+++ src/main/java/com/javarush/akishina/controller/LoginServlet.java
@@ -1,14 +1,17 @@
 package com.javarush.akishina.controller;
 
+import com.javarush.akishina.QuestSession;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
+import lombok.extern.slf4j.Slf4j;
 
 import java.io.IOException;
 
+@Slf4j
 @WebServlet("")
 public class LoginServlet extends HttpServlet {
 
@@ -16,17 +19,19 @@ public class LoginServlet extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
         req.getRequestDispatcher("/WEB-INF/index.jsp").forward(req, resp);
-
+        log.info("Запуск приложения, перевод на страницу входа.");
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
 
-        String username = req.getParameter("username");
-        HttpSession session = req.getSession();
-        session.setAttribute("userName", username);
+        String userName = req.getParameter("userName");
+        QuestSession questSession = new QuestSession(req.getSession());
+        questSession.setUserName(userName);
+        questSession.setClientIp(req.getRemoteAddr());
 
         resp.sendRedirect(req.getContextPath() + "/quests");
+        log.info("Пользователь ввел имя: {}, перенаправление на страницу квестов.", userName);
 
     }
 
